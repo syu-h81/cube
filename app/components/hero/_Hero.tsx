2ae99325f6c59085bc31639b07b4f72b1182f4e9
Danjo Syu <>
2026-04-28 12:26:01 +0900

    Heroコンポーネントの作成

---
+++ app/components/hero/_Hero.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import styles from "./hero.module.css";
+
+type Props = {
+  title: string;
+  description: string;
+  imageSrc?: string;
+};
+
+export default function Hero({ title, description, imageSrc }: Props) {
+  return (
+    <section className={styles.hero}>
+      <div className={`${styles.heroContainer} container`}>
+        <div className={styles.heroBody}>
+          <h2 className={styles.heroTitle}>{title}</h2>
+          <p className={styles.heroDescription}>{description}</p>
+        </div>
+        {imageSrc && (
+          <div className={styles.heroImage}>
+            <Image
+              src={imageSrc}
+              alt="Hero Image"
+              width={575}
+              height={498}
+              priority
+            />
+          </div>
+        )}
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/_Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
+        <Hero 
+          title="CUBE"
+          description="アウトプットしていくサイト"
+          imageSrc="/images/hero-img.png"
+        />
       </main>
       <Footer />
     </>
