2eeccc5754d48f1d7c51d2be45f5bed3a4f07955
Danjo Syu <>
2026-05-01 10:39:30 +0900

    blogトップページ作成

---
+++ app/blog/page.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import Link from "next/link";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import Hero from "../components/hero/_Hero";
+import Posts from "../components/post/Post";
+import styles from "../components/post/post.module.css";
+
+const posts = [
+  {
+    title: "ブログ記事タイトル",
+    imageSrc: "/images/news-01.png",
+    link: "/blog/1"
+  },
+  {
+    title: "ブログ記事タイトル",
+    imageSrc: "/images/news-02.png",
+    link: "/blog/2"
+  },
+  {
+    title: "ブログ記事タイトル",
+    imageSrc: "/images/news-03.png",
+    link: "/blog/3"
+  },
+  {
+    title: "ブログ記事タイトル",
+    imageSrc: "/images/news-04.png",
+    link: "/blog/4"
+  },
+  {
+    title: "ブログ記事タイトル",
+    imageSrc: "/images/news-05.png",
+    link: "/blog/5"
+  },
+  {
+    title: "ブログ記事タイトル",
+    imageSrc: "/images/news-06.png",
+    link: "/blog/6"
+  },
+]
+
+export default function Blog() {
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <Hero 
+          title="Blog"
+          description="Recent Posts"
+          variant="about"
+        />
+        <section className={styles.post}>
+          <div className={` ${styles.postContainer} container`}>
+            <div className={styles.postBody}>
+              {posts.map((post) => (
+                <Posts 
+                  key={post.imageSrc}
+                  title={post.title}
+                  imageSrc={post.imageSrc}
+                  link={post.link}
+                />
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <Footer />
+    </>
+  )
+}
