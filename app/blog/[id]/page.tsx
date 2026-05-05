63cbc26aebb3bdbc9d5d26fc9f7c727c30eccbaf
Danjo Syu <>
2026-05-05 09:43:20 +0900

    subHeroコンポーネントの作成

---
+++ app/blog/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function BlogPage() {
+  return (
+    <>
+      <Header />
+      <main className="main">
+      </main>
+      <Footer />
+    </>
+  )
+}
